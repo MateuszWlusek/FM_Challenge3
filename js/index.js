86772b0a5df8774599eed2dd63581c18348ebb1a
Mateusz Włusek <mateusz.wlusek@gmail.com>
2021-10-06 15:14:57 +0200

    Added simple logic to "Request Access" button. Few stylistic changes on website.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-console.log('Hello there!');
+const submit = document.querySelector('.submit_btn');
+const input = document.querySelector('input');
+
+console.log(input);
+submit.addEventListener('click', function (e) {
+    const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    e.preventDefault();
+    if (!input.value.match(validRegex)) {
+        this.parentElement.children[2].innerText = 'Oops! Please check your email';
+    } else {
+        this.parentElement.children[2].innerText = 'Request sent!';
+        input.value = '';
+    }
+})
